153f008c340f1b3f90eed8949a147994bee7a7e2
0xArdi-N <0x4rd1@gmail.com>
2024-05-28 14:08:18 +0700

    add nvim spectre

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,3 +37,16 @@ vim.keymap.set("n", "<space>r", ':lua require("dbee").toggle()<CR>', { noremap =
 
 -- vim.keymap.set('n', '<C-CR>', ':<CMD> lua require("dbee").execute(vim.api.nvim_get_current_line())<CR>')
 -- Set NeoTree position to float
+
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
--- lua/plugins/smart-split.lua
+++ lua/plugins/smart-split.lua
@@ -10,5 +10,11 @@ return {
     {
         "APZelos/blamer.nvim"
     },
-    { "EdenEast/nightfox.nvim" } -- lazy
+    {
+        "EdenEast/nightfox.nvim"
+    },
+    {
+        'nvim-pack/nvim-spectre'
+    }
+
 }
