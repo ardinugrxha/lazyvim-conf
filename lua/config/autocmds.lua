0085da8a537ea1a1a8987a7f92d65fbb1fbf049f
Ardi Nugraha <0x4rd1@gmail.com>
2024-09-25 22:18:16 +0700

    Add HBAC plugin for manage unused buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.opt.relativenumber = false
+require("hbac").setup({
+  autoclose = true, -- set autoclose to false if you want to close manually
+  threshold = 5, -- hbac will start closing unedited buffers once that number is reached
+  close_command = function(bufnr)
+    vim.api.nvim_buf_delete(bufnr, {})
+  end,
+  close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
+  telescope = {
+    -- See #telescope-configuration below
+  },
+})
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,6 +8,17 @@ return {
   },
 
   {
+    "uloco/bluloco.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      -- your optional config goes here, see below.
+      transparent = true
+    end,
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "onedark",
