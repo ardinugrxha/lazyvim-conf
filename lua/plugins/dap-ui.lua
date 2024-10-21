61f843317fd49f287182018494fad897dabbe25e
ardinugrxha <0x4rd1@gmail.com>
2024-10-21 19:05:27 +0700

    Dont close nvim dap if there is some error

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -6,4 +6,15 @@ return {
     { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
   },
   opts = {},
+  config = function(_, opts)
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup(opts)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({})
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close({})
+    end
+  end,
 }
